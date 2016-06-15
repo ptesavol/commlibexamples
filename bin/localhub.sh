84ca5a3e70c004919822b82133c46d98d3943779
Petri Savolainen <petri.savolainen@hiit.fi>
2016-06-15 10:32:53 +0300

    Local hub binary added

---
+++ bin/localhub.sh
@@ -0,0 +1,6 @@
+
+#ToDo insert below the one of your IP addresses that the mobile devices can contact to
+
+LISTENIP="192.168.1.104"
+
+node communicationhub.min.js --parenthost spaceify.net --parentport 1979 --port 1979 --host $LISTENIP
