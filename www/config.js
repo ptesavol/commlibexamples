bfe022a80e542f4d715a8a11eeb15bf982502a1b
Petri Savolainen <petri.savolainen@hiit.fi>
2016-06-20 09:38:59 +0300

    GROUP_ID constant moved to config.js

---
+++ www/config.js
@@ -0,0 +1,7 @@
+//Communication group name for development use without the Application Server
+				
+var GROUP_NAME = "petris_own_group";
+		
+var SERVER_ADDRESS = {host: "spaceify.net", port: 1979};
+var WEBRTC_CONFIG = {"iceServers":[{url:"stun:kandela.tv"},{url :"turn:kandela.tv", username:"webrtcuser", credential:"jeejeejee"}]};
+			
